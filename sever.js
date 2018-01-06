ce215ef4f50732096ecf17fe395cfb251049969e
Jesus Pineda <jpineda@NSL-JPINEDAs-MacBook-Pro.local>
2018-01-05 19:54:30 -0700

    Using express static

--- sever.js
+++ sever.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var http = require('http');
+var morgan = require('morgan');
+var path = require('path');
 
 var app = express();
 
-app.use(function(req, res, next){
-    console.log("In comes a " + req.method + " to " + req.url);
-    next();
-})
+var publicPath = path.resolve(__dirname,'public');
+app.use(express.static(publicPath));
+
+app.use(morgan('short'));
 
 app.use(function(req,res){
     res.writeHead(200, {"content-type":"text/plain"})
